21a8c817f92bd2a8f5f7859759c4d2bae3fa7c74
joecoker <joe_coker@hotmail.co.uk>
2018-11-28 16:52:20 +0000

    Add API connection

--- src/thermostat_jquery.js
+++ src/thermostat_jquery.js
@@ -3,14 +3,21 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
 
+  $('#city_submit').click(function() {
+    var city = $('#new_city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city +  '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      var location_temp = data.main.temp;
+      var location_city = data.name
+      $('#location_city').text(location_city);
+      $('#location_temp').text(location_temp);
+    });
+  });
 
   $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
   var location_temp = data.main.temp;
   var location_city = data.name
   $('#location_city').text(location_city);
   $('#location_temp').text(location_temp);
-
-
   });
 
 
